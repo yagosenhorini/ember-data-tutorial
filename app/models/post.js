07b8d6d6292d0f261543ead2f2e33087db108bef
yagosenhorini <yagosenhorini30@gmail.com>
2018-01-23 15:20:59 -0200

    Change userId to user

--- app/models/post.js
+++ app/models/post.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import { belongsTo } from 'ember-data/relationships';
 
 export default DS.Model.extend({
-    userId: belongsTo('user'),
+    user: belongsTo('user'),
     title: DS.attr('string'),
     body: DS.attr('string'),
 
