9f07a88aa93710c4ea47cba5628edf5078b36106
yagosenhorini <yagosenhorini30@gmail.com>
2018-01-23 15:30:31 -0200

    Create a forEach method for substitute post.userId to post.user on template

--- app/serializers/post.js
+++ app/serializers/post.js
@@ -4,7 +4,11 @@ export default DS.RESTSerializer.extend({
 
     normalizeResponse(store, primaryModelClass, payload, id, requestType) {
         payload = { posts: payload }
-        console.log(payload);
+        
+        payload.posts.forEach((post)=>{
+            post.user = post.userId;
+            delete post.userId;
+        })
        
         return this._super(store, primaryModelClass, payload, id, requestType);
     }
