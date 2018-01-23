f767909387cf811caaa934e0060af3ef89bbfcf8
yagosenhorini <yagosenhorini30@gmail.com>
2018-01-23 16:22:28 -0200

    Edit route passing params to show only the post associate with a unique id

--- app/posts/user/route.js
+++ app/posts/user/route.js
@@ -1,4 +1,12 @@
+import Ember from 'ember';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(params){
+        return this.store.query('post', { userId: params.id });
+    },
+
+    setupController(controller, model){
+        Ember.set(controller, 'posts', model);
+    }
 });
