dd773fcec88a394f5d861477fce4dad54b398ecf
yagosenhorini <yagosenhorini30@gmail.com>
2018-01-23 16:58:11 -0200

    Edit route to show only a unique post per id

--- app/posts/show/route.js
+++ app/posts/show/route.js
@@ -1,4 +1,11 @@
+import Ember from 'ember';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(params) {
+        return this.store.findRecord('post', params.id);
+    },
+    setupController(controller, model){
+        Ember.set(controller, 'post', model);
+    }
 });
