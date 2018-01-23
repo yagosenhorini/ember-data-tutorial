0346d95b808f15174a30c4b5733f8fe0b3780363
yagosenhorini <yagosenhorini30@gmail.com>
2018-01-23 13:43:58 -0200

    Insert host and path for type

--- app/adapters/post.js
+++ app/adapters/post.js
@@ -1,4 +1,8 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
+    host: 'https://jsonplaceholder.typicode.com',
+    pathForType() {
+        return 'posts';
+    }
 });
