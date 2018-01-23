f2cb29213f2221a0c0fc7a4a125540ad544b7040
yagosenhorini <yagosenhorini30@gmail.com>
2018-01-23 17:01:55 -0200

    Edit normalizeResponse for show a specific type of response, like a unique post per id, all posts for a unique id and all posts for all users

--- app/serializers/post.js
+++ app/serializers/post.js
@@ -4,12 +4,24 @@ export default DS.RESTSerializer.extend({
 
     normalizeResponse(store, primaryModelClass, payload, id, requestType) {
         payload = { posts: payload }
-        
-        payload.posts.forEach((post)=>{
+
+
+
+        return this._super(store, primaryModelClass, payload, id, requestType);
+    },
+
+    normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
+        payload.posts.user = payload.posts.userId;
+        delete payload.posts.userId;
+
+        return this._super(store, primaryModelClass, payload, id, requestType);
+    },
+
+    normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+        payload.posts.forEach((post) => {
             post.user = post.userId;
             delete post.userId;
-        })
-       
+        });
         return this._super(store, primaryModelClass, payload, id, requestType);
     }
 });
