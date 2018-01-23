58276c4a7c91a41a3c869b867e0238e76163ae15
yagosenhorini <yagosenhorini30@gmail.com>
2018-01-23 14:53:07 -0200

    Pass parameters of normalize to return

--- app/serializers/post.js
+++ app/serializers/post.js
@@ -3,7 +3,9 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
 
     normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+        payload = { posts: payload }
         console.log(payload);
-        return this._super(...arguments);
+       
+        return this._super(store, primaryModelClass, payload, id, requestType);
     }
 });
